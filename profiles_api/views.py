85137a2a16681fe70c395832f1d469132e97d8bd
MJMoquin <mjmoquin@gmail.com>
2021-02-07 21:17:07 -0600

    add search user function

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
@@ -127,3 +127,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
   authentication_classes = (TokenAuthentication,)
   permission_classes = (permissions.UpdateOwnProfile,)
+  
+  filter_backends = (filters.SearchFilter,)
+  search_fields = ('name', 'email',)
